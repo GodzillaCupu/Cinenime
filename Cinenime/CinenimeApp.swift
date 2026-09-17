80a802550b36920ba43fd6e682c36c8f2c38a754
Mac-Ery <prasetyo.djufri@gmail.com>
2026-09-17 07:15:46 +0700

    Initial Commit

---
+++ Cinenime/CinenimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CinenimeApp.swift
+//  Cinenime
+//
+//  Created by Ery Prasetyo on 17/09/26.
+//
+
+import SwiftUI
+
+@main
+struct CinenimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
