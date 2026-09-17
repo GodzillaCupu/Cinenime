2bee55d3dd027518dc362ab13b826dc6ab7663f4
Mac-Ery <prasetyo.djufri@gmail.com>
2026-09-17 17:32:30 +0700

    add : home screen view file

---
+++ Cinenime/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  Cinenime
+//
+//  Created by Ery Prasetyo on 17/09/26.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeView()
+}
