8a9c5c82c86ef328151267116ad89e1c67a1014a
Mac-Ery <prasetyo.djufri@gmail.com>
2026-09-21 11:51:27 +0700

    add : title data struct

---
+++ Cinenime/Title.swift
@@ -0,0 +1,15 @@
+//
+//  Title.swift
+//  Cinenime
+//
+//  Created by Ery Prasetyo on 21/09/26.
+//
+
+import Foundation
+
+struct Title{
+    var id: Int
+    var title: String
+    var overview: String
+    var posterPat: String
+}
