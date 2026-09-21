a97bb33bd569cdbeebb76a5d4cdd1f3bd6d8f029
Mac-Ery <prasetyo.djufri@gmail.com>
2026-09-21 12:30:31 +0700

    add : APIConfig json for source data, and APIConfig Swift for interprate data from json to swift ui

---
+++ Cinenime/APIConfig.swift
@@ -0,0 +1,30 @@
+//
+//  APIConfig.swift
+//  Cinenime
+//
+//  Created by Ery Prasetyo on 21/09/26.
+//
+
+import Foundation
+
+struct APIConfig: Decodable{
+    let tmdbBaseURL: String
+    let tmdbAPIKey: String
+    
+    //Singleton Desain Pattern - Start
+    
+    static let shared : APIConfig = {
+        guard let url = Bundle.main.url(forResource: "APIConfig", withExtension: "json") else{
+         fatalError("API CONFIG (JSON) is missing or invalid")
+        }
+        
+        do{
+            let data = try Data(contentsOf: url)
+            return try JSONDecoder().decode(APIConfig.self, from: data)
+        } catch{
+            fatalError("Failed to load or decode API CONFIG (JSON)")
+        }
+    }()
+    
+    //Singleton Desain Pattern - END
+}
