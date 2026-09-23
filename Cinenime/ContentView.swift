2f401211ca4dba727ed37604cb1e665221bef542
Mac-Ery <prasetyo.djufri@gmail.com>
2026-09-24 04:32:52 +0700

    remove : content view (.onappear func) for debuging only

--- Cinenime/ContentView.swift
+++ Cinenime/ContentView.swift
@@ -26,11 +26,6 @@ struct ContentView: View {
             {
                 Text(Constants.downloadString)
             }
-        }.onAppear{
-            if let config = APIConfig.shared{
-                print (config.tmdbAPIKey)
-                print (config.tmdbBaseUrl)
-            }
         }
     }
 }
---
+++ Cinenime/DataFetcher.swift
@@ -0,0 +1,8 @@
+//
+//  DataFetcher.swift
+//  Cinenime
+//
+//  Created by Ery Prasetyo on 24/09/26.
+//
+
+import Foundation
